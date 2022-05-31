20d7868fa954d17da79de860ead9cd2cffac44a5
joao <jzati@mitsidi.com>
2022-05-31 13:12:13 -0300

    Adicionando script.py com chunksize

--- script.py
+++ script.py
@@ -1,5 +1,12 @@
 from decouple import config
+import pandas as pd
 
 # no arquivo .env insira o camilho para baixar a planilha
 DIR_EXCEL = config("DIR_EXCEL")
-print(DIR_EXCEL)
+
+path_excel_2022_02 = DIR_EXCEL + 'Consumo_horario_2022_02.csv'
+
+ch_2022_02 = pd.read_csv(path_excel_2022_02, sep=';', chunksize=1000000)
+
+for chunk in ch_2022_02:
+    print(chunk)
